da3fa0980fa6f6562730427c9f4c51082dc7be43
예준 <ohman0705@naver.com>
2022-05-20 11:25:36 +0900

    Random

--- src/components/Random/index.tsx
+++ src/components/Random/index.tsx
@@ -1,24 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, {useState} from "react";
 import styled from "@emotion/styled";
 
-const MenuList = ["피자 🍕", "치킨 🍗", "햄버거 🍔", "라면 🍜", "떡볶이 🌶", "굶기 🤮"];
-export const RandomMenu: any = (MenuList[Math.floor(Math.random() * MenuList.length)]);
-
 const Print = () => {
+   const MenuList = ["피자 🍕", "치킨 🍗", "햄버거 🍔", "라면 🍜", "떡볶이 🌶", "굶기 🤮"];
+   const RandomMenu: any = (MenuList[Math.floor(Math.random() * MenuList.length)]);
    const [viewCalendar, setViewCalendar] = useState(false)
-   const PrintMenu = () => {
-      return {RandomMenu};
-   }
+
    if (viewCalendar == true) {
-      console.log("TRUE");
-      return <PrintMenu/>;
+      console.log("true");
+      return {RandomMenu}.RandomMenu;
    }
 
-   return (
-     <>
-        <button onClick={() => setViewCalendar(true)}>여기야</button>
-     </>
-   )
+   return <button onClick={() => setViewCalendar(true)}>여기야</button>
 
 }
 
