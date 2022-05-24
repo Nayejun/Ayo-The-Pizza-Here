3dabbf56f735f1714811e27eb80d561a6a16be4a
예준 <ohman0705@naver.com>
2022-05-24 15:18:24 +0900

    import error 2

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import * as S from "./style";
-import ShowRandomMenu from "../Random";
+import ShowRandomMenu from "../Random/index";
 
 const MainPage = () => {
    return (
-     <>
-        <S.Positioner>
-           <S.Wrapper>
-              <S.Text>오늘 먹을 메뉴는,</S.Text>
-              <ShowRandomMenu/>
-              <S.Title>입니다.</S.Title>
-           </S.Wrapper>
-        </S.Positioner>
-     </>
+      <>
+         <S.Positioner>
+            <S.Wrapper>
+               <S.Text>오늘 먹을 메뉴는,</S.Text>
+               <ShowRandomMenu />
+               <S.Title>입니다.</S.Title>
+            </S.Wrapper>
+         </S.Positioner>
+      </>
    );
-}
+};
 
-export default MainPage
+export default MainPage;
