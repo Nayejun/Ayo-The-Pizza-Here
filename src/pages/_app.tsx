7af7c3f92d3695674f409b97ee4398a7019451fe
예준 <ohman0705@naver.com>
2022-05-24 15:17:14 +0900

    import error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Global } from "@emotion/react";
 import { GlobalStyles } from "../style/GlobalStyle";
-import Landing from "../components/Landing";
-import Main from "../components/Main";
+import Landing from "../components/Landing/index";
+import Main from "../components/Main/index";
 import Head from "next/head";
 
 const MyApp = () => {
