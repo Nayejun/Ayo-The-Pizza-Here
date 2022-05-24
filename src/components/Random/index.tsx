d8f9d5ee4caddc4fe2172c1a7d8eaa5ab729b0bb
예준 <ohman0705@naver.com>
2022-05-24 16:07:01 +0900

    fixes????????

--- src/components/Random/index.tsx
+++ src/components/Random/index.tsx
@@ -1,17 +1,25 @@
-import React, {useState} from "react";
-import {Positioner} from "./style";
+import React, { useState } from "react";
+import { Positioner } from "./style";
 
 const ShowRandomMenu = () => {
-   const MenuList = ["피자 🍕", "치킨 🍗", "햄버거 🍔", "라면 🍜", "떡볶이 🌶", "굶기 🤮"];
-   const RandomMenu: any = (MenuList[Math.floor(Math.random() * MenuList.length)]);
-   const [view, setView] = useState<boolean>(false)
+   const MenuList = [
+      "피자 🍕",
+      "치킨 🍗",
+      "햄버거 🍔",
+      "라면 🍜",
+      "떡볶이 🌶",
+      "굶기 🤮",
+   ];
+   const RandomMenu: any =
+      MenuList[Math.floor(Math.random() * MenuList.length)];
+   const [view, setView] = useState<boolean>(false);
 
    if (view == true) {
       console.log("true");
-      return <Positioner>{RandomMenu}</Positioner>
+      return <Positioner>{RandomMenu}</Positioner>;
    }
 
-   return <button onClick={() => setView(true)}>메뉴 정하기</button>
-}
+   return <button onClick={() => setView(true)}>메뉴 정하기</button>;
+};
 
-export default ShowRandomMenu
+export default ShowRandomMenu;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Global } from "@emotion/react";
 import { GlobalStyles } from "../style/GlobalStyle";
 import Landing from "../components/Landing/index";
-import MainPage from "../components/Main/index";
+import MainPage from "../components/Menu/index";
 import Head from "next/head";
 
 const MyApp = () => {
