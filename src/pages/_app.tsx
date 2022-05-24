632529a52aae4f64da97f11ee434f300fe63b16b
예준 <ohman0705@naver.com>
2022-05-24 16:12:40 +0900

    hi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Global } from "@emotion/react";
 import { GlobalStyles } from "../style/GlobalStyle";
 import Landing from "../components/Landing/index";
-import MainPage from "../components/Menu/index";
 import Head from "next/head";
 
 const MyApp = () => {
@@ -14,7 +13,6 @@ const MyApp = () => {
          </Head>
          <Global styles={GlobalStyles} />
          <Landing />
-         <MainPage />
       </>
    );
 };
