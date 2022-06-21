4cc58efb291e0e870133cfdf6bbd848a21375403
4YJ <ohman0705@naver.com>
2022-06-21 14:53:01 +0900

    Menu List State Management

--- src/components/Random/index.tsx
+++ src/components/Random/index.tsx
@@ -2,24 +2,23 @@ import React, { useState } from "react";
 import { Positioner } from "./style";
 
 const ShowRandomMenu = () => {
-   const MenuList = [
-      "피자 🍕",
-      "치킨 🍗",
-      "햄버거 🍔",
-      "라면 🍜",
-      "떡볶이 🌶",
-      "굶기 🤮",
-   ];
-   const RandomMenu: any =
-      MenuList[Math.floor(Math.random() * MenuList.length)];
-   const [view, setView] = useState<boolean>(false);
+  const [menu, setMenu] = useState([
+    "피자 🍕",
+    "치킨 🍗",
+    "햄버거 🍔",
+    "라면 🍜",
+    "떡볶이 🌶",
+    "굶기 🤮",
+  ]);
+  const RandomMenu: any = menu[Math.floor(Math.random() * menu.length)];
+  const [view, setView] = useState<boolean>(false);
 
-   if (view == true) {
-      console.log("true");
-      return <Positioner>{RandomMenu}</Positioner>;
-   }
+  if (view == true) {
+    console.log("true");
+    return <Positioner>{RandomMenu}</Positioner>;
+  }
 
-   return <button onClick={() => setView(true)}>메뉴 정하기</button>;
+  return <button onClick={() => setView(true)}>메뉴 정하기</button>;
 };
 
 export default ShowRandomMenu;
